f3d01105b039d39df93e8b940c26af6cb206126e
David Weber <david.weber@w3tec.ch>
2017-05-08 00:05:08 +0200

    Correct title styling

--- src/app/modules/child-router/child-router.tsx
+++ src/app/modules/child-router/child-router.tsx
@@ -6,26 +6,23 @@ import { ChildRouterRoutes } from './child-router.routes';
 export class ChildRouter extends React.Component<any, any> {
   public render(): JSX.Element {
     return (
-      <div>
-        <h1>Child Router</h1>
-        <section>
-          <div>
-            <div className='col-md-2'>
-              <h2>Links</h2>
-              <ul className='well nav nav-pills nav-stacked'>
-                {ChildRouterRoutes.routes.map((route, index) => (
-                  <li key={ index }>
-                    <NavLink to={route.path} activeClassName='active'>{route.title}</NavLink>
-                  </li>
-                ))}
-              </ul>
-            </div>
-            <div className='col-md-10'>
-              <ChildRouterRoutes />
-            </div>
+      <section>
+        <div>
+          <div className='col-md-2'>
+            <h2>Child Router</h2>
+            <ul className='well nav nav-pills nav-stacked'>
+              {ChildRouterRoutes.routes.map((route, index) => (
+                <li key={index}>
+                  <NavLink to={route.path} activeClassName='active'>{route.title}</NavLink>
+                </li>
+              ))}
+            </ul>
           </div>
-        </section >
-      </div >
+          <div className='col-md-10'>
+            <ChildRouterRoutes />
+          </div>
+        </div>
+      </section>
     );
   }
 }
