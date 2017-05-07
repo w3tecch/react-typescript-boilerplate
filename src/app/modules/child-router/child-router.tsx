275be0db4c45c979c965df7ae7e7f27b4e7fb1b5
David Weber <david.weber@w3tec.ch>
2017-05-08 00:02:56 +0200

    Add recursive routes

--- src/app/modules/child-router/child-router.tsx
+++ src/app/modules/child-router/child-router.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import { NavLink } from 'react-router-dom';
 
 import { ChildRouterRoutes } from './child-router.routes';
 
@@ -7,8 +8,24 @@ export class ChildRouter extends React.Component<any, any> {
     return (
       <div>
         <h1>Child Router</h1>
-        <ChildRouterRoutes />
-      </div>
+        <section>
+          <div>
+            <div className='col-md-2'>
+              <h2>Links</h2>
+              <ul className='well nav nav-pills nav-stacked'>
+                {ChildRouterRoutes.routes.map((route, index) => (
+                  <li key={ index }>
+                    <NavLink to={route.path} activeClassName='active'>{route.title}</NavLink>
+                  </li>
+                ))}
+              </ul>
+            </div>
+            <div className='col-md-10'>
+              <ChildRouterRoutes />
+            </div>
+          </div>
+        </section >
+      </div >
     );
   }
 }
