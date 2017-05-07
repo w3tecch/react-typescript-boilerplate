b2e33263b005a773a6825392c4520178c28a0363
David Weber <david.weber@w3tec.ch>
2017-05-08 00:05:23 +0200

    Add style for github users

--- src/app/modules/users/users.tsx
+++ src/app/modules/users/users.tsx
@@ -3,7 +3,22 @@ import * as React from 'react';
 export class Users extends React.Component<any, any> {
   public render(): JSX.Element {
     return (
-      <h1>Users</h1>
+      <section>
+        <h2>Users</h2>
+        <div className='row'>
+          <div className='col-sm-6 col-md-3 card-container au-animate'>
+            <div className='card'>
+              <div className='avatar'>
+                <img crossorigin ref='image' />
+              </div>
+              <div className='content'>
+                <p className='name'>Name</p>
+                <p><a target='_blank' className='btn btn-default' href='user.getHtmlUrl()'>Contact</a></p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
     );
   }
 }
